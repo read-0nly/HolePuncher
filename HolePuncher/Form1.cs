0ff613e2a48168f6715589759f449e49f1a5c602
obsol <33932119+read-0nly@users.noreply.github.com>
2020-04-14 20:09:54 -0400

    HolePuncher demo ready

--- HolePuncher/Form1.cs
+++ HolePuncher/Form1.cs
@@ -12,9 +12,27 @@ namespace HolePuncher
 {
     public partial class Form1 : Form
     {
+        ConnectionManager cm = new ConnectionManager();
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = cm.Endpoint.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cm.sendString("Follow the white rabbit", textBox2.Text.Trim());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Encoding.ASCII.GetString(cm.receiveBytes());
+        }
+
+        private void button3_Click(object sender, EventArgs e){        
+            cm.sendString("Follow the white rabbit", textBox1.Text.Trim());
+            textBox3.Text = Encoding.ASCII.GetString(cm.receiveBytes());
         }
     }
 }
